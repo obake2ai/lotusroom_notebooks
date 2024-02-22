f8e0d69a65176e475179e75d11e3202020cf15a7
obake2ai <obake2ai@gmail.com>
2024-02-22 23:02:43 +0900

    switch to 5.3

--- config/.ipynb_checkpoints/sd_config-checkpoint.py
+++ config/.ipynb_checkpoints/sd_config-checkpoint.py
@@ -1,12 +1,14 @@
 init_image_list=[]
+init_image_list.extend([f"/root/Share/LOTUSROOM/ref/HTR_S24LOOK-{i}.jpg" for i in range(1, 31)])
+
 sd_params = {
     'width': 860,
     'height': 860,
-    'min_strength': 0.63,
-    'max_strength': 0.95,
-    'min_lora_weight': 0.7,
+    'min_strength': 0.5,
+    'max_strength': 0.8,
+    'min_lora_weight': 0.3,
     'max_lora_weight': 1.0,
     'guidance_scale': 7.0,
-    'num_inference_steps': 20,
-    'init_image_list': init_image_list.extend([f"/root/Share/LOTUSROOM/ref/HTR_S24LOOK-{i}.jpg" for i in range(1, 31)])
+    'num_inference_steps': 24,
+    'init_image_list': init_image_list,
 }
